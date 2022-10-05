61db6d66c9b39af4cabbeb74e139e6585e7a5662
BrennanBertola <brennan.bertola@comcast.net>
2022-10-04 18:38:05 -0600

    kinda done? rotations works movement isn't camera based

--- Lab5/Lab5-OpenGL.py
+++ Lab5/Lab5-OpenGL.py
@@ -18,6 +18,11 @@ except:
 DISPLAY_WIDTH = 500.0
 DISPLAY_HEIGHT = 500.0
 
+CURR_X = 0
+CURR_Y = -4
+CURR_Z = -15
+CURR_DEG = 0
+
 def init(): 
     glClearColor (0.0, 0.0, 0.0, 0.0)
     glShadeModel (GL_FLAT)
@@ -80,8 +85,12 @@ def display():
 
     
     #Your Code Here
-    
-    
+
+    glLoadIdentity()
+    gluPerspective(90, DISPLAY_WIDTH / DISPLAY_HEIGHT, 0, 1)
+    glRotated(CURR_DEG, 0, 1, 0)
+    glTranslated(CURR_X, CURR_Y, CURR_Z)
+
     drawHouse()
 
     
@@ -89,29 +98,47 @@ def display():
     
 
 def keyboard(key, x, y):
+    global CURR_X
+    global CURR_Y
+    global CURR_Z
+    global CURR_DEG
     
     if key == chr(27):
         import sys
         sys.exit(0)
-  
+
+    # basic testing not movement not based off of camera direction
+
     if key == b'w':
         print("W, move forward")
+        CURR_Z += 1
     elif key == b'a':
         print("A, move left")
+        CURR_X += 1
     elif key == b's':
         print("S, move back")
+        CURR_Z -= 1
     elif key == b'd':
         print("D, move right")
+        CURR_X -= 1
     elif key == b'q':
         print("Q, turn left")
+        CURR_DEG -= 1
     elif key == b'e':
         print("E, turn right")
+        CURR_DEG += 1
     elif key == b'r':
         print("R, move up")
+        CURR_Y -= 1
     elif key == b'f':
         print("F, move down")
-    elif key == b'd':
+        CURR_Y += 1
+    elif key == b'h':
         print("H, return home")
+        CURR_X = 0
+        CURR_Y = -4
+        CURR_Z = -15
+        CURR_DEG = 0
     elif key == b'o':
         print("O, orthographic projection")
     elif key == b'p':
